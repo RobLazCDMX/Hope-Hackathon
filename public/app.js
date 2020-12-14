e0049993ffb4e7733673e00ca9410c4f26c68181
Mateo ospina <mateoospina@macbook-pro-3.home>
2020-12-14 18:06:53 -0500

    first commit

---
+++ public/app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const fetch = require("node-fetch");
+
+app.get("/covid", async (req, res) => {
+  const covidData = await fetch(
+    "https://api.covid19api.com/live/country/mexico"
+  );
+
+  const covidJson = await covidData.json();
+
+  res.json(covidJson);
+});
+
+app.get("/", (req, res) => {
+  res.send("Root API");
+});
+
+// Server
+app.listen(3000, (req, res) => {
+  console.log("Server is running");
+});
